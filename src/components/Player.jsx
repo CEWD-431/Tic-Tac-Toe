5656745644c95ade803a0c0c65b459699fd714ec
KritikaSharmaKS <sh.kritika01@gmail.com>
2024-02-22 16:11:48 -0500

    comment exercise

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,6 +1,16 @@
-
+import { useState } from "react";
 
 export default function Player ({ name, symbol }) {
+
+    const [ isEditing, setIsEditing ] = useState(false);
+
+    /* 
+        add a function that's triggered when the button is clicked
+        change isEditing to true in that function
+        show  <span className="player-name"> only when isEditing is false
+        show <input> element if isEditing is true
+    */
+
     return (
         <li>
             <span>

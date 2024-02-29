1c50b1bed69eb9c55fdbc2c888e0cfdd63ba60e2
KritikaSharmaKS <sh.kritika01@gmail.com>
2024-02-29 10:15:03 -0500

    disable button if clicked once + rename col var

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -32,9 +32,10 @@ export default function GameBoard({ onSelectSquare, turns }) {
       {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
-            {row.map((col, colIndex) => (
+            {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>{col}</button>
+                <button onClick={() => onSelectSquare(rowIndex, colIndex)}
+                disabled={playerSymbol!==null}>{playerSymbol}</button>
               </li>
             ))}
           </ol>

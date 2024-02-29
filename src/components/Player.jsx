e107d1560d81241adce588bc33e92ca79be8cc98
KritikaSharmaKS <sh.kritika01@gmail.com>
2024-02-28 20:23:08 -0500

    [WIP] working with state to edit player name

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -4,20 +4,17 @@ export default function Player ({ name, symbol }) {
 
     const [ isEditing, setIsEditing ] = useState(false);
 
-    /* 
-        add a function that's triggered when the button is clicked
-        change isEditing to true in that function
-        show  <span className="player-name"> only when isEditing is false
-        show <input> element if isEditing is true
-    */
+    function EditClickHandler () {
+        setIsEditing(true);
+    }
 
     return (
         <li>
             <span>
-                <span className="player-name">{ name }</span>
+                {isEditing ? <input type="text" required /> : <span className="player-name">{ name }</span>}
                 <span className="player-symbol">{ symbol }</span>
             </span>
-            <button>Edit</button>
+            <button onClick={EditClickHandler}>{isEditing ? "Save" : "Edit"}</button>
         </li>
     )
 } 

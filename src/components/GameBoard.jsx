3cd4c1511a367679c4d4438e1d7b098473307484
KritikaSharmaKS <sh.kritika01@gmail.com>
2024-02-28 21:24:21 -0500

    [WIP] start working on the game board component

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,18 @@
+
+export default function GameBoard () {
+//game board structure
+    return (
+        <ol id="game-board">
+            <li>
+                <ol>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ol>
+            </li>
+            <li></li>
+            <li></li>
+        </ol>
+    )
+
+}

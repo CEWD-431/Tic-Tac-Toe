b9467a3d0c200bab2864fa3d0639f607f682f06b
KritikaSharmaKS <sh.kritika01@gmail.com>
2024-02-28 21:56:27 -0500

    best practice to update object state with a deep copy

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
@@ -5,14 +7,25 @@ const initialGameBoard = [
 ];
 
 export default function GameBoard() {
+  const [ gameBoard, setGameBoard ] = useState(initialGameBoard);
+
+  function selectSquareHandler (rowIndex, colIndex) {
+    setGameBoard((prevGameBoard) => {
+        // prevGameBoard[rowIndex][colIndex] = 'X'; //wrong way to update object state
+        const updatedGameBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+        updatedGameBoard[rowIndex][colIndex] = 'X';
+        return updatedGameBoard;
+    })
+  }
+
   return (
     <ol id="game-board">
-      {initialGameBoard.map((row, rowIndex) => (
+      {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((col, colIndex) => (
               <li key={colIndex}>
-                <button>{col}</button>
+                <button onClick={() => selectSquareHandler(rowIndex, colIndex)}>{col}</button>
               </li>
             ))}
           </ol>

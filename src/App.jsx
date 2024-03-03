826c495f80e90ee6cfb728c815a730e0be353380
KritikaSharmaKS <sh.kritika01@gmail.com>
2024-03-02 23:34:52 -0500

    create a deep copy of gameBoard array

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
   const [ gameTurns, setGameTurns ] = useState([]);
   const activePlayer = deriveActivePlayer(gameTurns);
 
-  const gameBoard = initialGameBoard;
+  const gameBoard = [...initialGameBoard.map(innerArray => [...innerArray])];
   for (const turn of gameTurns) {
       const { square, player } = turn;
       const { row, col } = square;
